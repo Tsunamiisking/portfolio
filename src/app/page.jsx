5995eeba7b71a56552b9f1d8a4238553e7ddd062
Allen Douglas <allenissabigboi@gmail.com>
2025-03-29 13:53:29 -0700

    feat: initialize Next.js project with configuration files, global styles, and components

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+"use client";
+import Image from "next/image";
+import HeroSection from "./components/HeroSection";
+import VelocityText from "./components/HireMe";
+
+export default function Home() {
+  return (
+    
+      <HeroSection />
+  );
+}
