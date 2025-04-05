ebe46c7b32f17df4275a71655a3a3d61d07ae81a
Allen Douglas <allenissabigboi@gmail.com>
2025-04-05 09:35:33 -0700

    feat: refactor Header component to use NavMenu array for navigation links

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,14 +1,20 @@
+"use client"
 import React from 'react'
+const NavMenu = [
+  { name: "Hero", href: "#hero" },
+  { name: "Work", href: "#work" },
+  { name: "About", href: "#about" },
+  { name: "Contact", href: "#contact" },
+];
 
 
 const Header = () => {
   return (
     <div className="flex md:grid md:grid-cols-2 justify-center items-center md:justify-between backgroundButton md:px-20 w-full py-5 rounded-xl manrope-bold sticky top-5">
         <div className='flex justify-between md:justify-start gap-10 order-2 md:order-1 font'>
-            <div className=''>Hero</div>
-            <div>Work</div>
-            <div>About</div>
-            <div>Contact</div>
+            {NavMenu.map((item, index) => {
+                return <div key={index} className='flex items-center'><a href={item.href}>{item.name}</a></div>
+            })}
         </div>
         <div className='md:justify-end order-1 md:order-2 md:flex hidden'>
             socials
@@ -16,5 +22,4 @@ const Header = () => {
     </div>
 )
 }
-
 export default Header
