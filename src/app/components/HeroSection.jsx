7fdbb78ef360ec03a9d9e00fd0e04ad46d82eb66
Allen Douglas <allenissabigboi@gmail.com>
2025-04-06 15:03:38 -0700

    completed hero section

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -5,13 +5,13 @@ import HeroCard from "./HeroCard";
 const HeroSection = () => {
   const word = "< Hello World />";
   return (
-    <div className="min-h-screen w-full grid grid-cols-1 md:grid-cols-2 justify-center items-center">
+    <div className="min-h-screen w-full grid grid-cols-1 md:grid-cols-2 justify-center items-center mt-20 md:mt-0 px-5 md:pl-20 gap-10">
       <div className="">
         {/* Text Section */}
         <h1 className="md:text-left text-center text-3xl outfit-bold mb-5 textColor">
           Hi, I'm <span className="blueText">Allen Douglas 👋</span>
         </h1>
-        <p className="text-2xl md:text-left text-center textColor mb-5">
+        <p className="text-2xl md:text-left text-center textColor mb-5 outfit-bold">
           A passionate Software Engineering student, Web Full-Stack and Mobile
           Dev.
         </p>
@@ -20,7 +20,7 @@ const HeroSection = () => {
           and performance.
         </p>
       </div>
-      <div className="hidden md:block">
+      <div className="">
         <HeroCard />
       </div>
     </div>
