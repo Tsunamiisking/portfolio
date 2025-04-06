6680514cde2891627b0e5c71e50fca0e391ef3c4
Allen Douglas <allenissabigboi@gmail.com>
2025-04-06 15:54:42 -0700

    Add waving hand SVG and integrate animation in HeroSection component
    - Added a new SVG file for a waving hand graphic.
    - Updated HeroSection component to include the waving hand SVG.
    - Implemented hover animation using framer-motion for the waving hand image.
    - Adjusted text layout and styling for better presentation.

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,21 +1,41 @@
 import React from "react";
 import Header from "./Header";
 import HeroCard from "./HeroCard";
+import { motion } from "framer-motion";
 
+const wavingHand = "/assets/images/wavinghang.svg";
 const HeroSection = () => {
   const word = "< Hello World />";
   return (
     <div className="min-h-screen w-full grid grid-cols-1 md:grid-cols-2 justify-center items-center mt-20 md:mt-0 px-5 md:pl-20 gap-10">
       <div className="">
         {/* Text Section */}
-        <h1 className="md:text-left text-center text-3xl outfit-bold mb-5 textColor">
-          Hi, I'm <span className="blueText">Allen Douglas 👋</span>
+        <div className="md:justify-start text-center flex justify-center items-center md:items-start">
+        <h1 className="text-3xl outfit-bold textColor">
+          Hello World{" "}
         </h1>
-        <p className="text-2xl md:text-left text-center textColor mb-5 outfit-bold">
+        <motion.img
+            initial={{rotate: [0,0,0, 0]}}
+            // animate={{rotate: [0, -45, 35, 0]}}
+            whileHover={{rotate: [0, -45, 35, 0]}}
+            exit={{rotate: [0, 0, 0, 0]}}
+            transition={{duration: 1, repeat: false }}
+            // transition={.2}
+            src={wavingHand}
+            alt="Waving Hand"
+            height={40}
+            width={40}
+          > 
+          </motion.img>
+        </div>
+        <h1 className="md:text-left text-center text-3xl outfit-bold mb-5 textColor">
+        <span className="mt-5 block blueText"> I'm Allen Douglas</span>
+        </h1>
+        <p className="text-2xl md:text-left text-center text-gray-300 mb-5 outfit-bold">
           A passionate Software Engineering student, Web Full-Stack and Mobile
           Dev.
         </p>
-        <p className="text-xl md:text-left text-center textColor mb-5">
+        <p className="text-xl md:text-left text-center text-gray-300">
           I build efficient, user-focused solutions with a strong eye for design
           and performance.
         </p>
