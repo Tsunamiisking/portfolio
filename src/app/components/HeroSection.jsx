d2e11ac000a31852b7b921cfd3a6af9917e13e2d
Allen Douglas <allenissabigboi@gmail.com>
2025-04-06 14:51:21 -0700

    feat: enhance HeroSection text for clarity and update global background color for consistency

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -8,15 +8,20 @@ const HeroSection = () => {
     <div className="min-h-screen w-full grid grid-cols-1 md:grid-cols-2 justify-center items-center">
       <div className="">
         {/* Text Section */}
-        <h1 className="md:text-left text-center text-3xl outfit-bold mb-5 textColor">Hi, I'm Allen</h1>
-        <p>
-          A passionate Software Engineering student, Web Full-Stack and Mobile Dev. I
-          build efficient, user-focused solutions with a strong eye for design
+        <h1 className="md:text-left text-center text-3xl outfit-bold mb-5 textColor">
+          Hi, I'm <span className="blueText">Allen Douglas 👋</span>
+        </h1>
+        <p className="text-2xl md:text-left text-center textColor mb-5">
+          A passionate Software Engineering student, Web Full-Stack and Mobile
+          Dev.
+        </p>
+        <p className="text-xl md:text-left text-center textColor mb-5">
+          I build efficient, user-focused solutions with a strong eye for design
           and performance.
         </p>
       </div>
       <div className="hidden md:block">
-        <HeroCard  />
+        <HeroCard />
       </div>
     </div>
   );
