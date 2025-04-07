f5b35b1fb289f1a5e18f81b4bf5a38edae0319ce
Allen Douglas <allenissabigboi@gmail.com>
2025-04-07 16:38:10 -0700

    feat: replace div with motion.div in WorkSection for animation effects

--- src/app/components/WorkSection.jsx
+++ src/app/components/WorkSection.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
 
 const WorkSection = () => {
   return (
-    <div className='h-screen w-full flex justify-center items-center yellow-card opacity-40 rounded-2xl'>
-      
-    </div>
-  )
-}
+    <motion.div
+      whileInView={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      transition={{ duration: 5}}
+      className="h-screen w-full flex justify-center items-center yellow-card opacity-40 rounded-2xl"
+    ></motion.div>
+  );
+};
 
-export default WorkSection
+export default WorkSection;
