cc30d2be78ed485b67aea3ea16e59ed77316aa20
Allen Douglas <allenissabigboi@gmail.com>
2025-04-09 14:13:28 -0700

    feat: add WorkCard component and integrate PetPal image in WorkSection; update styling for improved layout

---
+++ src/app/components/WorkCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WorkCard = () => {
+  return (
+    <div className='w-full yellow-card'>
+        
+    </div>
+  )
+}
+
+export default WorkCard
--- src/app/components/WorkSection.jsx
+++ src/app/components/WorkSection.jsx
@@ -1,14 +1,32 @@
 import React from "react";
 import { motion } from "framer-motion";
 
+const PetPal = "../assets/images/petpal.png";
 const WorkSection = () => {
   return (
     <motion.div
-      whileInView={{ opacity: 1 }}
-      initial={{ opacity: 0 }}
-      transition={{ duration: 5}}
-      className="h-screen w-full flex justify-center items-center yellow-card opacity-40 rounded-2xl"
-    ></motion.div>
+      // whileInView={{ opacity: 1 }}
+      // initial={{ opacity: 0 }}
+      // transition={{ duration: 5}}
+      className="mt-20 h-screen w-full rounded-2xl"
+    >
+      <div className="grid grid-cols-1 ">
+        <WorkCard />
+      </div>
+    </motion.div>
+  );
+};
+
+const WorkCard = () => {
+  return (
+    <div className="grid grid-col-1 w-full p-5 yellow-card">
+      <div className="w-full md:w-1/2">
+        <img src={PetPal} alt="" />
+      </div>
+      <div className="">
+        <h1></h1>
+      </div>
+    </div>
   );
 };
 
