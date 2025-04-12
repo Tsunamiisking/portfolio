81dd2077ac007a58c0206c0af1811643d4d69be1
Allen Douglas <allenissabigboi@gmail.com>
2025-04-11 20:48:57 -0700

    feat: integrate PetPal project details into WorkSection; add stack data and update styling for improved presentation

--- src/app/components/WorkSection.jsx
+++ src/app/components/WorkSection.jsx
@@ -1,8 +1,14 @@
 import * as React from "react";
 import { motion } from "framer-motion";
-import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+import OpenInNewIcon from "@mui/icons-material/OpenInNew";
 import { PieChart } from "@mui/x-charts/PieChart";
-import NorthEastIcon from '@mui/icons-material/NorthEast';
+import {
+  messagingstack,
+  petpalstack,
+  logbookstack,
+  marketmobilestack,
+} from "../data/stacks";
+import NorthEastIcon from "@mui/icons-material/NorthEast";
 const VALUEFORMATER = (item) => `${item.value}%`;
 
 const PetPal = "../assets/images/petpal.png";
@@ -16,61 +22,55 @@ const WorkSection = () => {
       className="mt-20 h-screen w-full rounded-2xl"
     >
       <div className="flex w-full ">
-        <WorkCard />
+        <WorkCard
+          src={PetPal}
+          title="PetPal"
+          description="A Django-based web application designed to simplify pet adoption.
+            Built with Django, SQLite, HTML, and CSS, the platform allows users
+            to create an account, browse a variety of pets, and adopt their
+            perfect companion with ease."
+          stackdata={petpalstack}
+        />
       </div>
     </motion.div>
   );
 };
 
-const WorkCard = () => {
-  const technology = [
-    {
-      label: "Python",
-      value: 500,
-    },
-    {
-      label: "HTML/CSS",
-      value: 200,
-    },
-    {
-      label: "Sqlite3",
-      value: 200,
-    },
-    {
-      label: "Others",
-      value: 100,
-    },
-  ];
+const WorkCard = ({ stackdata, src, title, description }) => {
   return (
-    <div className="flex justify-center flex-col md:flex-row  w-full p-5 yellow-card">
+    <div className="flex justify-center flex-col md:flex-row  w-full p-5 no-border-card">
       {/* Image DIV */}
       <div className="w-full mb-5 md:w-90 md:mb-0 lg:w-full flex-1/3">
-        <img src={PetPal} alt="" />
+        <img src={src} alt="" />
       </div>
 
       <div className="flex md:ml-5 flex-2/3 align-middle">
         <div className="">
           <a href="">
-          <h1 className="text-xl md:text-left text-left text-gray-300 mb-5 outfit-bold">
-            PetPal <OpenInNewIcon sx={{ fontSize: 21 }} />
-          </h1>
+            <h1 className="text-xl md:text-left text-left text-gray-300 mb-5 outfit-bold">
+              {/* PetPal */}
+              {title} <OpenInNewIcon sx={{ fontSize: 21 }} />
+            </h1>
           </a>
           <p className="text-md text-left text-gray-300">
-            A Django-based web application designed to simplify pet adoption.
-            Built with Django, SQLite, HTML, and CSS, the platform allows users
-            to create an account, browse a variety of pets, and adopt their
-            perfect companion with ease.
+            {description}
           </p>
         </div>
-        <div className="flex flex-col justify-center ml-2"> 
+        <div className="flex flex-col justify-center ml-2">
           <PieChart
             colors={["#2a9d8f", "#0d2833", "#e76f51", "#e9c46a"]}
             series={[
               {
-                // paddingAngle: 5,
+                paddingAngle: 5,
                 innerRadius: 30,
                 outerRadius: 20,
-                data: technology,
+                data: stackdata,
+                highlightScope: { fade: "global", highlight: "item" },
+                faded: {
+                  innerRadius: 40,
+                  additionalRadius: -30,
+                  color: "gray",
+                },
               },
             ]}
             margin={{ right: 5 }}
---
+++ src/app/data/stacks.js
@@ -0,0 +1,78 @@
+const petpalstack = [
+    {
+      label: "Python",
+      value: 500,
+    },
+    {
+      label: "HTML/CSS",
+      value: 200,
+    },
+    {
+      label: "Sqlite3",
+      value: 200,
+    },
+    {
+      label: "Others",
+      value: 100,
+    },
+  ];
+
+  const logbookstack = [
+    {
+      label: "Javasctipt",
+      value: 400,
+    },
+    {
+      label: "React/NextJS",
+      value: 200,
+    },
+    {
+      label: "LLM integration",
+      value: 300,
+    },
+    {
+      label: "Others",
+      value: 100,
+    },
+  ];
+
+  const messagingstack = [
+    {
+      label: "Javasctipt",
+      value: 400,
+    },
+    {
+      label: "React",
+      value: 200,
+    },
+    {
+      label: "Firebase",
+      value: 300,
+    },
+    {
+      label: "Others",
+      value: 100,
+    },
+  ];
+
+
+  const marketmobilestack = [
+    {
+      label: "React Native",
+      value: 400,
+    },
+    {
+      label: "Expo",
+      value: 200,
+    },
+    {
+      label: "Firebase",
+      value: 300,
+    },
+    {
+      label: "Others",
+      value: 100,
+    },
+  ];
+
+  export {petpalstack, logbookstack, messagingstack, marketmobilestack}
